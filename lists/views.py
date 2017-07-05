2261235556cd36a6c2ad5c67e84af6b3856e0e4b
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-07-05 17:39:01 +0800

    Basic view now returns blank HTML

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home_page():
-	pass
+def home_page(HttpRequest):
+	return HttpResponse('<html><title>To-Do Lists</title></html>')
 
 # Create your views here.
