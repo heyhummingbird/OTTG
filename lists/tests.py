25aa2ecec3c54ce1149144fb75089c679ad45560
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-07-06 16:00:17 +0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -10,17 +10,17 @@ from lists.views import home_page
 #         self.assertEqual(3, 3)
 
 class HomePageTest(TestCase):
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
-
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		html = response.content.decode('utf8')
-		self.assertTrue(html.startswith('<html>'))
-		self.assertIn('<title>To-Do Lists</title>', html)
-		self.assertTrue(html.endswith('</html>'))
+    def test_home_page_returns_correct_html(self):
+#        request = HttpRequest()
+#        found = resolve('/')
+#        response = found.func(request)
+        response = self.client.get('/')
+#        html = response.content.decode('utf8')
+#        print(repr(html))
+#        self.assertTrue(html.startswith('<html>'))
+#        self.assertIn('<title>To-Do Lists</title>', html)
+#        self.assertTrue(html.strip().endswith('</html>'))
+        self.assertTemplateUsed(response, 'home.html')
 
 
 # Create your tests here.
--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(HttpRequest):
-	return HttpResponse('<html><title>To-Do Lists</title></html>')
+#    return HttpResponse('<html><title>To-Do Lists</title></html>')
+    return render(HttpRequest, 'home.html')
 
 # Create your views here.
