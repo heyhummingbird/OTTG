576b745027c9f766eb3c28b98f751ac12549d344
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-07-10 21:19:16 +0800

    use function to check list

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,17 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        ## <tr> </tr> specifies a row
+        ## rows is a "list"
+        self.assertIn(
+            row_text,
+            [row.text for row in rows], 
+            f"New to-do item didn't appear in the table. Contents were:\n{table.text}"
+        )
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -34,32 +45,20 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        ## <tr> </tr> specifies a row
-        ## rows is a "list"
-        self.assertIn(
-            '1: Read OS',
-            [row.text for row in rows], 
-            f"New to-do item didn't appear in the table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_list_table('1: Read OS')
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')
+        ## We have to redefine inputbox after the page updates
         inputbox.send_keys('Write songs')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2: Write songs',
-            [row.text for row in rows], 
-            f"New to-do item didn't appear in the table. Contents were:\n{table.text}"
-        )
+        self.check_for_row_in_list_table('1: Read OS')
+        self.check_for_row_in_list_table('2: Write songs')
 
         self.fail('Finish the test!')
 
