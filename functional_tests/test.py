8b346d66ae697fbff10d8f5cb8399e00d017253d
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-07-12 02:28:25 +0800

    add multiple user scene function test

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+
+MAX_WAIT = 5
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,16 +15,25 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        ## <tr> </tr> specifies a row
-        ## rows is a "list"
-        self.assertIn(
-            row_text,
-            [row.text for row in rows], 
-            f"New to-do item didn't appear in the table. Contents were:\n{table.text}"
-        )
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                ## <tr> </tr> specifies a row
+                ## rows is a "list"
+                self.assertIn(
+                    row_text,
+                    [row.text for row in rows], 
+                    f"New to-do item didn't appear in the table. Contents were:\n{table.text}"
+                )
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.1)
+
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -47,8 +59,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Read OS')
+        self.wait_for_row_in_list_table('1: Read OS')
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -56,14 +67,12 @@ class NewVisitorTest(LiveServerTestCase):
         ## We have to redefine inputbox after the page updates
         inputbox.send_keys('Write songs')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         
-        self.check_for_row_in_list_table('1: Read OS')
-        self.check_for_row_in_list_table('2: Write songs')
+        self.wait_for_row_in_list_table('1: Read OS')
+        self.wait_for_row_in_list_table('2: Write songs')
 
-        self.fail('Finish the test!')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
@@ -73,3 +82,41 @@ class NewVisitorTest(LiveServerTestCase):
         
         # Satisfied, she goes back to sleep
 
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Read OS')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Read OS')
+
+        edith_url = self.browser.current_url
+        self.assertRegex(edith_url, '/lists/.+')
+        ## '.' for any character, '+' for 1 or more repititions
+
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_content = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Read OS', page_content)
+        self.assertNotIn('Write songs', page_content)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Communicate with Testing Goat')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Communicate with Testing Goat')
+
+        francis_url = self.browser.current_url
+        self.assertRegex(francis_url, '/lists/.+')
+        self.assertNotEqual(francis_url, edith_url)
+
+        page_content = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Read OS', page_content)
+        self.assertNotIn('Write songs', page_content)
