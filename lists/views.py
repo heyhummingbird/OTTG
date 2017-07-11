c5c409f91169de2098da14444f312fd82e82f0d9
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-07-11 13:59:31 +0800

    Redirect after POST, and show all items with number

--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from lists.models import Item
 
 def home_page(request):
 #    return HttpResponse('<html><title>To-Do Lists</title></html>')
+    if request.method == 'POST':
+        new_item_text = request.POST['input_item']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+
     return render(request, 'home.html', 
-        {'new_item_text': request.POST.get('input_item', '')})
+        {'items': Item.objects.all()})
 
 # Create your views here.
