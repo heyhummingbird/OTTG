0ee1f6cbac3579358cdf6e9128eea4fafbcd7fab
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-07-13 02:28:44 +0800

    create list object for model

--- lists/tests.py
+++ lists/tests.py
@@ -3,12 +3,13 @@ from django.urls import resolve
 from django.http import HttpRequest
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
 class ListViewTest(TestCase):
     def test_displays_all_list_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/the-only-list/')
         print(response)
@@ -22,7 +23,9 @@ class ListViewTest(TestCase):
 
 class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
-        self.client.post('/lists/new', data = {'input_item': 'A new list item'})
+        list_ = List()
+        self.client.post('/lists/new', data = {
+            'input_item': 'A new list item'})
 #        print(repr(response.content.decode()))
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
@@ -35,23 +38,34 @@ class NewListTest(TestCase):
 #        self.assertEqual(response['location'], '/lists/the-only-list/')
         self.assertRedirects(response, '/lists/the-only-list/')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
 
+        saved_list = List.objects.first()
+#        self.assertEqual(saved_list.count(), 1)
+        self.assertEqual(saved_list, list_)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.list, list_)
 
 class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
