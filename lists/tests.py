05a8ba80eb5c43f735359a1f504a6e72208bb176
楊舒瑄 <EllieYang@yangshuxuandeMacBook-Air.local>
2017-07-12 17:04:05 +0800

    add new view for new list

--- lists/tests.py
+++ lists/tests.py
@@ -17,8 +17,23 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list')
+        response = self.client.get('/lists/the-only-list/')
         self.assertTemplateUsed(response, 'list.html')
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data = {'input_item': 'A new list item'})
+#        print(repr(response.content.decode()))
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirect_after_POST(self):
+        response = self.client.post('/lists/new', data = {'input_item': 'A new list item'})
+#        print(response)
+#        self.assertEqual(response.status_code, 302)
+#        self.assertEqual(response['location'], '/lists/the-only-list/')
+        self.assertRedirects(response, '/lists/the-only-list/')
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -50,22 +65,3 @@ class HomePageTest(TestCase):
 #        self.assertIn('<title>To-Do Lists</title>', html)
 #        self.assertTrue(html.strip().endswith('</html>'))
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-    def test_can_save_a_POST_request(self):
-        self.client.post('/', data = {'input_item': 'A new list item'})
-#        print(repr(response.content.decode()))
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirect_after_POST(self):
-        response = self.client.post('/', data = {'input_item': 'A new list item'})
-#        print(response)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list/')
-
-# Create your tests here.
--- lists/views.py
+++ lists/views.py
@@ -4,14 +4,14 @@ from lists.models import Item
 
 def home_page(request):
 #    return HttpResponse('<html><title>To-Do Lists</title></html>')
-    if request.method == 'POST':
-        new_item_text = request.POST['input_item']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list/')
-
     return render(request, 'home.html')
 
 def view_list(request):
     return render(request, 'list.html', {
         'items': Item.objects.all()
     })
+
+def new_list(request):
+    new_item_text = request.POST['input_item']
+    Item.objects.create(text=new_item_text)
+    return redirect('/lists/the-only-list/')
